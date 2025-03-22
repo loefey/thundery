895bceca7117b197379e95f5c24101aebfb6896a
loefey (nolan) <loefey@proton.me>
2025-03-22 05:06:55 -0400

    Added source

---
+++ README.md
@@ -0,0 +1,57 @@
+# Thundery
+
+Thundery is a command-line application that fetches and displays weather information from the OpenWeatherMap API.
+
+![thundery](https://pub-772556e86c514a789d81677bd605749d.r2.dev/thundery.jpg)
+
+Thundery's idea, structure and design is based off [Rainy](https://github.com/liveslol/rainy) but writen in Rust instead of Python making it 25% faster!!
+
+![thunder vs rainy](https://pub-772556e86c514a789d81677bd605749d.r2.dev/thunderyandrainy.jpg)
+
+## Installation
+
+### Package managers:
+
+Currently, on none. But I'm trying to add it to some!
+
+### From source:
+
+1. Clone the repository:
+
+   ```sh
+   git clone https://github.com/loefey/thundery.git
+   cd thundery
+   ```
+
+2. Build the project using Cargo:
+   ```sh
+   cargo build --release
+   ```
+
+## Configuration
+
+The application uses a configuration file located at `~/.config/thundery/thundery.toml` (or `C:\Users\<username>\AppData\Roaming\thundery\thundery.toml` on Windows). If the configuration file does not exist, a default one will be created.
+
+### Configuration Options
+
+- `api_key`: Your OpenWeatherMap API key.
+- `city`: The city for which to fetch weather data.
+- `units`: Units for temperature and wind speed (`metric` or `imperial`).
+- `timeplus`: Hours to add to sunrise and sunset times.
+- `timeminus`: Hours to subtract from sunrise and sunset times.
+- `showcityname`: Whether to display the city name (`true` or `false`).
+- `showdate`: Whether to display the current date (`true` or `false`).
+- `timeformat`: Time format for sunrise and sunset times (`24` or `12`).
+
+Example configuration:
+
+```toml
+api_key = "your_api_key"
+city = "London"
+units = "metric"
+timeplus = 0
+timeminus = 0
+showcityname = true
+showdate = true
+timeformat = "24"
+```
---
+++ src/main.rs
@@ -0,0 +1,268 @@
+use reqwest::blocking::get;
+use serde::{ Deserialize, Serialize };
+use serde_json::Value;
+use chrono::{ DateTime, Utc, TimeZone, Duration };
+use std::fs;
+use dirs::home_dir;
+use std::fs::File;
+use std::io::Write;
+
+#[derive(Deserialize, Serialize)]
+struct Config {
+    api_key: String,
+    city: String,
+    units: String,
+    timeplus: i64,
+    timeminus: i64,
+    showcityname: bool,
+    showdate: bool,
+    timeformat: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            api_key: String::new(),
+            city: String::new(),
+            units: String::from("metric"),
+            timeplus: 0,
+            timeminus: 0,
+            showcityname: false,
+            showdate: false,
+            timeformat: String::from("24"),
+        }
+    }
+}
+
+fn read_config() -> Config {
+    let config_path = if cfg!(windows) {
+        let mut path = dirs::config_dir().expect("Failed to get config directory");
+        path.push("thundery");
+        path.push("thundery.toml");
+        path
+    } else {
+        let mut path = home_dir().expect("Failed to get home directory");
+        path.push(".config");
+        path.push("thundery");
+        path.push("thundery.toml");
+        path
+    };
+
+    if !config_path.exists() {
+        let default_config = Config::default();
+        let toml_string = toml
+            ::to_string(&default_config)
+            .expect("Failed to serialize default config");
+        fs::create_dir_all(config_path.parent().unwrap()).expect(
+            "Failed to create config directory"
+        );
+        let mut file = File::create(&config_path).expect("Failed to create config file");
+        file.write_all(toml_string.as_bytes()).expect("Failed to write default config to file");
+
+        let config_location = config_path.display().to_string();
+        println!("No config detected, config made at {}.", config_location);
+    }
+
+    let config_content = fs::read_to_string(config_path).expect("Failed to read config file");
+    toml::from_str(&config_content).expect("Failed to parse config file")
+}
+
+fn main() {
+    let config = read_config();
+
+    let url = format!(
+        "https://api.openweathermap.org/data/2.5/weather?q={}&units={}&APPID={}",
+        config.city,
+        config.units,
+        config.api_key
+    );
+
+    let response = get(&url).expect("Failed to send request");
+
+    if response.status().is_success() {
+        let weather_data: Value = response.json().expect("Failed to parse JSON");
+
+        let weather = weather_data["weather"][0]["main"].as_str().unwrap_or("Unknown");
+        let temp = weather_data["main"]["temp"].as_f64().unwrap_or(0.0);
+        let wind_speed = weather_data["wind"]["speed"].as_f64().unwrap_or(0.0);
+        let sunrise = weather_data["sys"]["sunrise"].as_i64().unwrap_or(0);
+        let sunset = weather_data["sys"]["sunset"].as_i64().unwrap_or(0);
+
+        let windspeedunits = match config.units.as_str() {
+            "metric" => "m/s",
+            "imperial" => "mph",
+            _ => "m/s",
+        };
+
+        let temp_unit = match config.units.as_str() {
+            "metric" => "°C",
+            "imperial" => "°F",
+            _ => "K",
+        };
+
+        let temp_str = format!("{:.1}{}", temp, temp_unit);
+        let wind_speed_str = format!("{:.1} {}", wind_speed, windspeedunits);
+
+        let sunrise_datetime: DateTime<Utc> = Utc.timestamp_opt(sunrise, 0).unwrap();
+        let sunset_datetime: DateTime<Utc> = Utc.timestamp_opt(sunset, 0).unwrap();
+
+        let adjusted_sunrise =
+            sunrise_datetime + Duration::hours(config.timeplus) - Duration::hours(config.timeminus);
+        let adjusted_sunset =
+            sunset_datetime + Duration::hours(config.timeplus) - Duration::hours(config.timeminus);
+
+        let time_format = match config.timeformat.as_str() {
+            "12" => "%I:%M %p",
+            _ => "%H:%M",
+        };
+
+        let sunrisestring = adjusted_sunrise.format(time_format).to_string();
+        let sunsetstring = adjusted_sunset.format(time_format).to_string();
+
+        let date = if config.showdate {
+            let now = Utc::now();
+            now.format("%x").to_string()
+        } else {
+            String::new()
+        };
+
+        let date_label = if config.showdate { "Date: " } else { "" };
+        let date_value = if config.showdate { date } else { String::new() };
+
+        let output = match weather {
+            "Clear" =>
+                format!(
+                    r#"               {}
+     \   /     Weather: clear
+      .-.      Temperature: {}
+   ‒ (   ) ‒   Wind speed: {}
+      `-᾿      Sunrise: {}
+     /   \     Sunset: {}
+               {}{}"#,
+                    if config.showcityname {
+                        format!("City: {}", config.city)
+                    } else {
+                        String::new()
+                    },
+                    temp_str,
+                    wind_speed_str,
+                    sunrisestring,
+                    sunsetstring,
+                    date_label,
+                    date_value
+                ),
+            "Clouds" =>
+                format!(
+                    r#"                 {}
+       .--.      Weather: cloudy
+    .-(    ).    Temperature: {}
+   (___.__)__)   Wind speed: {}
+                 Sunrise: {}
+                 Sunset: {}
+                 {}{}"#,
+                    if config.showcityname {
+                        format!("City: {}", config.city)
+                    } else {
+                        String::new()
+                    },
+                    temp_str,
+                    wind_speed_str,
+                    sunrisestring,
+                    sunsetstring,
+                    date_label,
+                    date_value
+                ),
+            "Rain" =>
+                format!(
+                    r#"                 {}
+       .--.      Weather: rainy
+    .-(    ).    Temperature: {}
+   (___.__)__)   Wind speed: {}
+    ʻ‚ʻ‚ʻ‚ʻ‚ʻ    Sunrise: {}
+                 Sunset: {}
+                 {}{}"#,
+                    if config.showcityname {
+                        format!("City: {}", config.city)
+                    } else {
+                        String::new()
+                    },
+                    temp_str,
+                    wind_speed_str,
+                    sunrisestring,
+                    sunsetstring,
+                    date_label,
+                    date_value
+                ),
+            "Snow" =>
+                format!(
+                    r#"                 {}
+       .--.      Weather: snowy
+    .-(    ).    Temperature: {}
+   (___.__)__)   Wind speed: {}
+    * * * * *    Sunrise: {}
+                 Sunset: {}
+                 {}{}"#,
+                    if config.showcityname {
+                        format!("City: {}", config.city)
+                    } else {
+                        String::new()
+                    },
+                    temp_str,
+                    wind_speed_str,
+                    sunrisestring,
+                    sunsetstring,
+                    date_label,
+                    date_value
+                ),
+            "Thunderstorm" =>
+                format!(
+                    r#"                 {}
+       .--.      Weather: stormy
+    .-(    ).    Temperature: {}
+   (___.__)__)   Wind speed: {}
+        /_       Sunrise: {}
+         /       Sunset: {}
+                 {}{}"#,
+                    if config.showcityname {
+                        format!("City: {}", config.city)
+                    } else {
+                        String::new()
+                    },
+                    temp_str,
+                    wind_speed_str,
+                    sunrisestring,
+                    sunsetstring,
+                    date_label,
+                    date_value
+                ),
+            _ =>
+                format!(
+                    r#"                 {}
+       .--.      Weather: {}
+    .-(    ).    Temperature: {}
+   (___.__)__)   Wind speed: {}
+                 Sunrise: {}
+                 Sunset: {}
+                 {}{}"#,
+                    if config.showcityname {
+                        format!("City: {}", config.city)
+                    } else {
+                        String::new()
+                    },
+                    weather,
+                    temp_str,
+                    wind_speed_str,
+                    sunrisestring,
+                    sunsetstring,
+                    date_label,
+                    date_value
+                ),
+        };
+
+        println!("{}", output);
+    } else {
+        eprintln!(
+            "Failed to fetch weather data: your api key and city are probably missing from the config file."
+        );
+    }
+}
